814ab254e6e0a9ed5b76e0c9187d1bf1bb937706
goreleaserbot <goreleaser@carlosbecker.com>
2022-01-14 21:11:02 +0000

    Brew formula update for hcr version v0.0.2

--- Formula/hcr.rb
+++ Formula/hcr.rb
@@ -5,20 +5,20 @@
 class Hcr < Formula
   desc "GitHub pages helm chart releaser."
   homepage "https://github.com/pete911/hcr"
-  version "0.0.1"
+  version "0.0.2"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/pete911/hcr/releases/download/v0.0.1/hcr_0.0.1_darwin_arm64.tar.gz"
-      sha256 "c14fde6f7c454d7e09042c99831dd4a67ca26d159795d9f68732942b396bf2f5"
+    if Hardware::CPU.intel?
+      url "https://github.com/pete911/hcr/releases/download/v0.0.2/hcr_0.0.2_darwin_amd64.tar.gz"
+      sha256 "56c2e85113fd9a920cdfa47e90cafc7deec92ef1a3395f85363737692500a92e"
 
       def install
         bin.install "hcr"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/pete911/hcr/releases/download/v0.0.1/hcr_0.0.1_darwin_amd64.tar.gz"
-      sha256 "0cd35ebd7a30516db27ab982f8bd5c8d27e13a1d524fe7abb5577b1725dfc328"
+    if Hardware::CPU.arm?
+      url "https://github.com/pete911/hcr/releases/download/v0.0.2/hcr_0.0.2_darwin_arm64.tar.gz"
+      sha256 "682d07a1a7e9112d7965402b9fae0fe298178012b0fee7cff24dfca302a04e49"
 
       def install
         bin.install "hcr"
@@ -27,17 +27,17 @@ class Hcr < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/pete911/hcr/releases/download/v0.0.1/hcr_0.0.1_linux_amd64.tar.gz"
-      sha256 "9fd184e92cf48fd3c9dee3b3dccc8c9b10bb8681333649c52b7c8aa8afbf3ef2"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/pete911/hcr/releases/download/v0.0.2/hcr_0.0.2_linux_arm64.tar.gz"
+      sha256 "2075ca4e7bda77a821d585aaf237aef3ccccd8811dbe0850d8945e8e14cc4d7c"
 
       def install
         bin.install "hcr"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/pete911/hcr/releases/download/v0.0.1/hcr_0.0.1_linux_arm64.tar.gz"
-      sha256 "77986edc62d224a40a058b6bf8536d7105b71590b0631aa29983b52ddd41a479"
+    if Hardware::CPU.intel?
+      url "https://github.com/pete911/hcr/releases/download/v0.0.2/hcr_0.0.2_linux_amd64.tar.gz"
+      sha256 "6341cd8573a412d19b27a1d3de0b7018cbb5fdd7c5bdd97e5fae7aef13ffdf00"
 
       def install
         bin.install "hcr"
